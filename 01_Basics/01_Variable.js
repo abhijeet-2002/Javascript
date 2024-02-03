3ad3658ec4a84157e1242cc81e180f509f61f6ff
abhijeet-2002 <129104880+abhijeet-2002@users.noreply.github.com>
2024-02-03 09:34:26 +0000

    commit

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,15 @@
+const accountId = 725001
+let accountEmail = "abhijeet@gmail.com"
+var accountPassword = "54321"
+accountCity = "Bihar sharif"
+let accountState
+
+//acountId = 2 //  Not Allowed
+
+accountEmail = "bikku@gmail.com"
+accountPassword = "947100"
+accountCity = "Patna"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
